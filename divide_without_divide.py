2cd9e784bd191e9427f17aaec66911c2cb386ef6
coderock100 <vd.ssaathvik@gmail.com>
2024-11-22 22:15:50 +0530

    divide without dividing

--- divide_without_divide.py
+++ divide_without_divide.py
@@ -1,14 +1,14 @@
-def divide(ourdividend, ourDivisor):
-    sign = (-1 if((ourdividend < 0)^(ourDivisor < 0)) else 1)
+def divide(ourDividend, ourDivisor):
+    sign = (-1 if((ourDividend < 0)^(ourDivisor < 0)) else 1)
 
-    ourdividend = abs(ourdividend)
+    ourDividend = abs(ourDividend)
     ourDivisor = abs(ourDivisor)
 
     quotientnumber = 0
     tempnumber = 0
 
     for i in range(31, -1, -1):
-        if( tempnumber + (ourDivisor << i) <= ourdividend):
+        if( tempnumber + (ourDivisor << i) <= ourDividend):
             tempnumber += ourDivisor << i
             quotientnumber |= 1 << i
         print('number ', tempnumber, quotientnumber)
@@ -19,4 +19,4 @@ def divide(ourdividend, ourDivisor):
 
 a = int(input('enter a for a/b : '))
 b = int(input('enter b for a/b : '))
-print('result of ', a,'/',b,'is',divide(a,b))
+print('result of ', a,'/',b,' is ',divide(a,b))
